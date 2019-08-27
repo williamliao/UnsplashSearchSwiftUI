0d2ddf6c590a48ebe177e7d6db7a1c0e377ec76a
william <enehsiun@gmail.com>
2019-08-27 15:25:32 +0800

    Initial Commit

---
+++ HelloSwiftUI_ListViewTests/HelloSwiftUI_ListViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  HelloSwiftUI_ListViewTests.swift
+//  HelloSwiftUI_ListViewTests
+//
+//  Created by 雲端開發部-廖彥勛 on 2019/8/27.
+//  Copyright © 2019 雲端開發部-廖彥勛. All rights reserved.
+//
+
+import XCTest
+@testable import HelloSwiftUI_ListView
+
+class HelloSwiftUI_ListViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
